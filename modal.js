90597a70832da326a5f13454c1943fb7642791fe
Lily78190 <thms1601@gmail.com>
2023-09-04 14:33:54 +0200

    modification function addEventListener + création function validate()

--- modal.js
+++ modal.js
@@ -13,15 +13,13 @@ const modalCloseButton = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const form = document.forms["reserve"];
 const formDataFields = document.querySelectorAll(".formData");
-const inputFirst = document.getElementById('first');
-const inputLast = document.getElementById('last');
-const inputEmail = document.getElementById('email');
-const inputQuantity = document.getElementById('quantity');
-const inputBirthdate = document.getElementById('birthdate');
-const inputLocation = document.getElementById('location');
-const inputCheckbox = document.getElementById('checkbox1');
-
-
+const inputFirst = document.getElementById("first");
+const inputLast = document.getElementById("last");
+const inputEmail = document.getElementById("email");
+const inputQuantity = document.getElementById("quantity");
+const inputBirthdate = document.getElementById("birthdate");
+const radioInputs = document.querySelectorAll('input[name="location"]');
+const inputCheckbox = document.getElementById("checkbox1");
 
 // Sélectionne la div de confirmation
 const confirmationDiv = document.getElementById("confirmation-message");
@@ -44,91 +42,129 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-
 form.addEventListener("submit", function (event) {
-  event.preventDefault(); 
-
-  // Création obj pour stocker les données du formulaire
-  const formData = {
-    first : inputFirst.value,
-    last : inputLast.value,
-    email : inputEmail.value,
-    birthdate : inputBirthdate.value,
-    quantity : inputQuantity.value,
-    // location : inputLocation,
-    ckeckbox : inputCheckbox.checked,
-  };
-
-
-
-  // Validez les données
-  const firstName = formData["first"];
-  const lastName = formData["last"];
-  const email = formData["email"];
-  const quantity = formData["quantity"];
-  // const location = formData["location"];
-  // const checkbox1 = document.getElementById("checkbox1");
-
- for (const input in formData) {
- 
-    const inputValue = formData[input];
-    if (!isValid(input, inputValue)){
-      const field = document.getElementById(input).parentNode;
-      console.log(field)
-    }
-    
- }
-
-  // if (
-  //   firstName.trim().length < 2 ||
-  //   lastName.trim().length < 2 ||
-  //   !isValidEmail(email) ||
-  //   isNaN(quantity) ||
-  //   !location ||
-  //   !checkbox1.checked
-  // ) {
-  //   // Si la validation échoue msg d'erreur
-  //   alert("Veuillez remplir tous les champs correctement.");
-  // } else {
-  //   // Si la validation réussit, affichez le message de confirmation et fermez-le après un certain temps
-  //   confirmationDiv.innerText = "Validation réussie. Le formulaire a été envoyé.";
-  //   confirmationDiv.style.display = "block";
-
-  //   // Fermez la div de confirmation après 3 secondes (ajustez le délai au besoin)
-  //   setTimeout(function () {
-  //     confirmationDiv.style.display = "none";
-  //   }, 3000,);
-    
-
-  //   // soumission du form si tout est valide
-  //   // form.validate();
-  // }
+  event.preventDefault();
+  validate();
 });
 
-// Fonction de validation d'adresse e-mail simple
-function isValidEmail(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
 
-function isValid(inputName, inputValue) {
-  if(inputName === "first") {
-    return inputValue.trim().length >= 2;
+function validate() {
+  // Récupération des valeurs des champs
+  const firstName = inputFirst.value.trim();
+  const lastName = inputLast.value.trim();
+  const email = inputEmail.value.trim();
+  const Birthdate = inputBirthdate.value;
+  const quantity = inputQuantity.value.trim();
+  const checkbox1 = inputCheckbox.checked;
+
+  // Fonction de validation d'adresse e-mail simple
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
 
-  if(inputName === "last") {
-    return inputValue.trim().length >= 2;
+  // Vérification des conditions
+
+  const errors = [];
+  let isAnyRadioChecked = false;
+
+  if (firstName.length < 2) {
+    errors.push("Le prénom doit contenir au moins 2 caractères.");
+    inputFirst.classList.add("error");
+  } else {
+    inputFirst.classList.remove("error");
   }
 
-  if(inputName === "email") {
-    return isValidEmail(inputValue);
+  if (lastName.length < 2) {
+    errors.push("Le nom doit contenir au moins 2 caractères.");
+    inputLast.classList.add("error");
+  } else {
+    inputLast.classList.remove("error");
   }
 
-  if(inputName === "quantity") {
-    return !isNaN(inputValue);
+  if (!Birthdate) {
+    errors.push("Entrez votre date de naissance");
+    inputBirthdate.classList.add("error");
+  } else {
+    inputBirthdate.classList.remove("error");
   }
 
-  if(inputName === "location") {
-    return !!location;
+  if (!isValidEmail(email)) {
+    errors.push("L'adresse e-mail n'est pas valide.");
+    inputEmail.classList.add("error");
+  } else {
+    inputEmail.classList.remove("error");
+  }
+
+  if (!checkbox1) {
+    errors.push("Vous devez accepter les conditions d'utilisation");
+    inputCheckbox.classList.add("error");
+  } else {
+    inputCheckbox.classList.remove("error");
+  }
+
+  // Parcourez tous les input radio
+  for (const radioInput of radioInputs) {
+    if (radioInput.checked) {
+      isAnyRadioChecked = true;
+      break; // Si au moins un est coché, sortez de la boucle
+    }
+  }
+
+  // Vérifiez la variable isAnyRadioChecked
+  if (!isAnyRadioChecked) {
+    errors.push("Vous devez choisir une option");
+    radioInputs.forEach((radioInput) => {
+      radioInput.classList.add("error");
+    });
+  } else {
+    radioInputs.forEach((radioInput) => {
+      radioInput.classList.remove("error");
+    });
+  }
+
+  if (errors.length > 0) {
+    // Effacer les messages d'erreur précédents
+    document.querySelectorAll(".error-message").forEach((errorMessage) => {
+      errorMessage.textContent = "";
+    });
+
+    // Afficher les messages d'erreur sous les champs correspondants
+    if (errors.includes("Le prénom doit contenir au moins 2 caractères.")) {
+      document.getElementById("error-first").textContent =
+        "Le prénom doit contenir au moins 2 caractères.";
+    }
+
+    if (errors.includes("Le nom doit contenir au moins 2 caractères.")) {
+      document.getElementById("error-last").textContent =
+        "Le nom doit contenir au moins 2 caractères.";
+    }
+
+    if (errors.includes("L'adresse e-mail n'est pas valide.")) {
+      document.getElementById("error-email").textContent =
+        "L'adresse e-mail n'est pas valide.";
+    }
+
+    if (errors.includes("Entrez votre date de naissance")) {
+      document.getElementById("error-birthdate").textContent =
+        "Entrez votre date de naissance";
+    }
+
+    if (errors.includes("Vous devez choisir une option")) {
+      document.getElementById("error-inputRadio").textContent =
+        "Vous devez choisir une option";
+    }
+
+    if (errors.includes("Vous devez accepter les conditions d'utilisation")) {
+      document.getElementById("error-checkbox").textContent =
+        "Vous devez accepter les conditions d'utilisation";
+    }
+    // S'il y a des erreurs, n'affiche pas le message de confirmation
+    confirmationDiv.innerHTML = "";
+    form.style.display = "block";
+  } else {
+    // S'il n'y a pas d'erreurs, affiche le message de confirmation
+    confirmationDiv.innerHTML = "Formulaire soumis avec succès !";
+    form.style.display = "none"; // Masquer le formulaire
   }
 }
